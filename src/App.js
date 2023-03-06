9631b5d56ac1e311ccbe7b5614f7301b6bce0672
titilopebukola <titilopebukola@gmail.com>
2023-03-06 15:13:05 +0000

    Add location

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import "./App.css";
-// import axios from "axios";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [location, setLocation] = useState({});
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // function to get location data
+  async function getLocation() {
+    //                api url  end point                    api key                     location iq   formatting data with json
+    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${searchQuery}&format=json`;
+    const res = await axios.get(API);
+    console.log(res.data[0]);
+    setLocation(res.data[0]);
+  }
+
+  function handlesearch(event) {
+    setSearchQuery(event.target.value);
+  }
   return (
     <div className="App">
-      <p>Hello world!</p>
+      {location.display_name && (
+        <p>
+          {location.display_name} is at lat and lon: {location.lat} / {location.lon}
+        </p>
+      )}
+      <input onChange={handlesearch} placeholder="search for a city" />
+      <button onClick={getLocation}>Get Data</button>
     </div>
   );
 }
-
 export default App;
