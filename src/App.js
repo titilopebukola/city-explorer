50f0c0b4eb26cb1227ff4ebf4908124032d1c969
titilopebukola <titilopebukola@gmail.com>
2023-03-06 14:50:27 +0000

    Testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>Hello world!</p>
+    </div>
+  );
 }
 
 export default App;
